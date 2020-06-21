1a42b83a8e89a2d93178666e0c90db084fccaeb3
Sinan Talha KOŞAR <s.tlhakosar@gmail.com>
2020-06-21 20:02:44 +0300

    random.seed added

--- dataset_operations.py
+++ dataset_operations.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 from sklearn.model_selection import train_test_split
+import random
+random.seed(1234)
 
 
 path_to_dir = "../"
