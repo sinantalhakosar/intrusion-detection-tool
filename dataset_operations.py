4e0b08a98c35bf34dc583b55ffeaa3f45401c954
Sinan Talha KOŞAR <s.tlhakosar@gmail.com>
2020-06-21 16:35:46 +0300

    decision model trained

--- dataset_operations.py
+++ dataset_operations.py
@@ -1,10 +1,14 @@
 from os import listdir
+#from blessings import Terminal
+import sys
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 from sklearn.model_selection import train_test_split
 
+
 path_to_dir = "../"
+#term = Terminal()
 
 def dataset_count():
     return len(find_all_datasets(path_to_dir))
@@ -17,6 +21,8 @@ csvpaths = find_all_datasets(path_to_dir)
 
 def get_dataframe(dataset_number=0):
     #print(csvpaths[dataset_number])
+    #with term.location(y=dataset_number+1):
+    print("loading dataset:",dataset_number, "-> 0%")
     df = pd.read_csv(path_to_dir+csvpaths[dataset_number],low_memory=False)
     df = df.loc[:, ~df.columns.str.replace("(\.\d+)$", "").duplicated()]
     return df
@@ -30,52 +36,55 @@ def concat_dataframes(csvpaths=csvpaths):
     df = pd.concat(df_list)
     return df
 
-def dataset_normalizer(df):
+def dataset_normalizer(df, index):
     categorical_columns = list()
     label_list = list()
+    #with term.location(y=index+1):
+    #    sys.stdout.write("\033[K")
+    print("normalizing dataset:",index,"-> 0%")
     for col_name in df.columns:
-        if df[col_name].dtypes == 'object':
-            if col_name != " Label":
+        if df[col_name].dtypes == 'object' and col_name != " Label":
                 for i in range(0,len(df[col_name].values)):
-                    print ("\r Processing... {:.2f}".format(i*100./(df.shape[0]))+str("%"), end="")
                     try:
                         df[col_name].values[i] = float(df[col_name].values[i])
                     except:
                         #print(type(df[col_name].values[i]))
                         categorical_columns.append(col_name)
                         break
-    print(df[" Label"].unique())
-    if len(categorical_columns) > 0:
-        df_categorical_values = df[categorical_columns]
+    #print(df[" Label"].unique())
+    # if len(categorical_columns) > 0:
+    #     df_categorical_values = df[categorical_columns]
 
-        unique_protocol=sorted(df[' Label'].unique())
+    #     unique_protocol=sorted(df[' Label'].unique())
 
-        string1 = 'Label_'
-        unique_protocol2=[string1 + x for x in unique_protocol]
+    #     string1 = 'Label_'
+    #     unique_protocol2=[string1 + x for x in unique_protocol]
 
 
-        df_categorical_values_enc=df_categorical_values.apply(LabelEncoder().fit_transform)
+    #     df_categorical_values_enc=df_categorical_values.apply(LabelEncoder().fit_transform)
 
-        enc = OneHotEncoder(categories="auto")
-        df_categorical_values_encenc = enc.fit_transform(df_categorical_values_enc)
-        df_cat_data = pd.DataFrame(df_categorical_values_encenc.toarray(),columns=unique_protocol2)
+    #     enc = OneHotEncoder(categories="auto")
+    #     df_categorical_values_encenc = enc.fit_transform(df_categorical_values_enc)
+    #     df_cat_data = pd.DataFrame(df_categorical_values_encenc.toarray(),columns=unique_protocol2)
 
-        print(df_cat_data.head())
+    #     #print(df_cat_data.head())
 
-        newdf=df.join(df_cat_data)
-        for col_name in categorical_columns:
-            newdf.drop(col_name, axis=1, inplace=True)
-        df = newdf
+    #     newdf=df.join(df_cat_data)
+    #     for col_name in categorical_columns:
+    #         newdf.drop(col_name, axis=1, inplace=True)
+    #     df = newdf
         
     return df
 
 def label_replace(df):
-    labeldf = df[" Label"]
-    newlabeldf=labeldf.replace({v:k for k, v in enumerate(labeldf.unique())})
-    df[" Label"] = newlabeldf
+    for i,unique in enumerate(df.loc[:,[' Label']].drop_duplicates().values):
+        df = df.replace({unique[0]:i})
     return df
 
-def dataset_split_and_label_replace(df):
+def dataset_split_and_label_replace(df,index):
+    #with term.location(y=index+1):
+    #    sys.stdout.write("\033[K")
+    print("splitting dataset:",index,"-> 0%")
     train_df, test_df = train_test_split(df, test_size=0.2)
     train_df = label_replace(train_df)
     test_df = label_replace(test_df)
--- fs.py
+++ fs.py
@@ -1,30 +1,47 @@
 import pandas as pd
 import numpy as np
 import sys
+import time
+#from blessings import Terminal
 import multiprocessing 
 import sklearn
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 from sklearn import preprocessing
 from sklearn.feature_selection import SelectPercentile, f_classif
 from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import RFE
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
 
 import dataset_operations as dbo
 
 colNames = list()
+#term = Terminal()
 
+def fs_rfe(X_train, Y_train, colnames):
+    clf = DecisionTreeClassifier(random_state=0)
+    #rank all features, i.e continue the elimination until the last one
+    rfe = RFE(clf, n_features_to_select=1)
+    rfe.fit(X_train, Y_train)
+    print ("Features sorted by their rank:")
+    print (sorted(zip(map(lambda x: round(x, 4), rfe.ranking_), colnames)))
 
-def fs_percentile(X_train, Y_train):
+def fs_percentile(X_train, Y_train, X_test):
     global colNames
     scaler1 = preprocessing.StandardScaler().fit(X_train)
     X_train=scaler1.transform(X_train) 
+    scaler5 = preprocessing.StandardScaler().fit(X_test)
+    X_test=scaler5.transform(X_test) 
     np.seterr(divide='ignore', invalid='ignore');
     selector=SelectPercentile(f_classif, percentile=10)
-    selector.fit_transform(X_train,Y_train)
+    X_new = selector.fit_transform(X_train,Y_train)
     true=selector.get_support()
     newcolindex=[i for i, x in enumerate(true) if x]
     newcolname=list(colNames[i] for i in newcolindex)
     #print(newcolname)
-    return newcolname
+    return X_new,X_test,newcolname, newcolindex
 
 def fs_extra_trees_classifier(X_train, Y_train, importance_count):
     model = ExtraTreesClassifier(n_estimators=10)
@@ -40,9 +57,21 @@ def fs_extra_trees_classifier(X_train, Y_train, importance_count):
 
 def feature_selection(index, dataset):
     global colNames
-    df = dbo.get_dataframe(index)
-    df = dbo.dataset_normalizer(df)
-    train_df, test_df = dbo.dataset_split_and_label_replace(df)
+    df = dbo.concat_dataframes()
+    # with term.location(y=index+1):
+    #     sys.stdout.write("\033[K")
+    print("loading dataset:",index, "-> 100%")
+    time.sleep(1)
+    df = dbo.dataset_normalizer(df,index)
+    # with term.location(y=index+1):
+    #     sys.stdout.write("\033[K")
+    print("normalizing dataset:",index,"-> 100%")
+    time.sleep(1)
+    train_df, test_df = dbo.dataset_split_and_label_replace(df,index)
+    # with term.location(y=index+1):
+    #     sys.stdout.write("\033[K")
+    print("splitting dataset:",index,"-> 100%")
+    time.sleep(1)
 # train_df =  dbo.label_replace(train_df)
 # test_df = dbo.label_replace(test_df)
 #df = dbo.concat_dataframes()
@@ -50,22 +79,43 @@ def feature_selection(index, dataset):
     # print("//////////////")
     # print(test_df[" Label"].head())
 
+    # with term.location(y=index+1):
+    #     sys.stdout.write("\033[K")
+    print("feature selecting for dataset:",index,"-> 0%")
+    
     X_train = train_df.drop(' Label',1)
-    Y_train = train_df[" Label"]
+    Y_train = train_df.loc[:,[" Label"]]
     X_test = test_df.drop(' Label',1)
-    Y_test = test_df[" Label"]
+    Y_test = test_df.loc[:,[" Label"]]
+
     colNames=list(X_train)
 
     X_train, Y_train = dbo.clean_dataset(X_train, Y_train)
     X_test, Y_test = dbo.clean_dataset(X_test, Y_test)
 
-    percentile_features = fs_percentile(X_train,Y_train)
-    print(dataset)
-    print(percentile_features)
-    extra_trees_classifier_features = fs_extra_trees_classifier(X_train, Y_train,len(percentile_features))
-    print(extra_trees_classifier_features)
+    X_new, X_test, percentile_features, newcolindex = fs_percentile(X_train,Y_train.values.ravel(),X_test)
+    X_test2=X_test[:,newcolindex]
+    #fs_rfe(X_new,Y_train,percentile_features) #another feature selection method
+    #extra_trees_classifier_features = fs_extra_trees_classifier(X_train, Y_train,len(percentile_features))
+    # with term.location(y=index+1):
+    #     sys.stdout.write("\033[K")
+    print("selected features for dataset:",index, "-> ",percentile_features)
+    #print(percentile_features)
+    #print(extra_trees_classifier_features)
+    #print("------------")
 
-    print("------------")
+    clf_rfeDoS=DecisionTreeClassifier(random_state=0)
+    clf_rfeDoS.fit(X_new, Y_train)
+    Y_pred = clf_rfeDoS.predict(X_test2)
+
+    accuracy = cross_val_score(clf_rfeDoS, X_test2, Y_test, cv=10, scoring='accuracy',average='micro')
+    print("Accuracy: %0.5f (+/- %0.5f)" % (accuracy.mean(), accuracy.std() * 2))
+    precision = cross_val_score(clf_rfeDoS, X_test2, Y_test, cv=10, scoring='precision',average='micro')
+    print("Precision: %0.5f (+/- %0.5f)" % (precision.mean(), precision.std() * 2))
+    recall = cross_val_score(clf_rfeDoS, X_test2, Y_test, cv=10, scoring='recall',average='micro')
+    print("Recall: %0.5f (+/- %0.5f)" % (recall.mean(), recall.std() * 2))
+    f = cross_val_score(clf_rfeDoS, X_test2, Y_test, cv=10, scoring='f1',average='micro')
+    print("F-measure: %0.5f (+/- %0.5f)" % (f.mean(), f.std() * 2))
 
 #gridsearchcv kullan, false positive table ı kullan, r-1?
 
@@ -73,3 +123,4 @@ def feature_selection(index, dataset):
 for i, dataset in enumerate(dbo.find_all_datasets()):
     process = multiprocessing.Process(target=feature_selection, args=(i,dataset, ))
     process.start()
+    break
